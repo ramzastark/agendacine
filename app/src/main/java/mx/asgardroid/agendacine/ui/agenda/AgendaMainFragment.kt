918e8d27798accf3b71e4e0ff82068e513e7ca36
Frank Manzo <ramzastark@gmail.com>
2022-07-25 19:48:38 -0500

    Task | BP-007 | Added  init librarys and basic navigation | Frank Manzo

--- app/src/main/java/mx/asgardroid/agendacine/ui/agenda/AgendaMainFragment.kt
+++ app/src/main/java/mx/asgardroid/agendacine/ui/agenda/AgendaMainFragment.kt
@@ -1,6 +1,5 @@
-package mx.asgardroid.agendacine.ui.main
+package mx.asgardroid.agendacine.ui.agenda
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,24 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import mx.asgardroid.agendacine.R
+import mx.asgardroid.agendacine.databinding.FragmentAgendaMainBinding
 import mx.asgardroid.agendacine.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class AgendaMainFragment : Fragment() {
 
-    private lateinit var binding: FragmentMainBinding
+    private lateinit var binding: FragmentAgendaMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMainBinding.inflate(inflater)
+    ): View? {
+        binding = FragmentAgendaMainBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnAgenda.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_agendaMainFragment)
+        binding.btnNewContect.setOnClickListener {
+            findNavController().navigate(R.id.action_agendaMainFragment_to_addContactFragment)
         }
     }
 }
