72b652a34bcf4280404003fab400b4eaf15dc8ef
Frank Manzo <ramzastark@gmail.com>
2022-07-25 19:13:18 -0500

    Commit inicial

---
+++ app/src/main/java/mx/asgardroid/agendacine/MainActivity.kt
@@ -0,0 +1,18 @@
+package mx.asgardroid.agendacine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.asgardroid.agendacine.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
